d41c71e8d1b81f420358c65d84673892367dc712
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2023-02-17 22:12:45 +1300

    Added terraform scripts for WIF

---
+++ google/workspaces/modules/workflow_identity_federation/main.tf
@@ -0,0 +1,24 @@
+
+resource "google_service_account" "sa" {
+  project    = var.project_id
+  account_id = "test-github-sa"
+}
+
+resource "google_project_iam_member" "project" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
+module "oidc" {
+  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id  = var.project_id
+  pool_id     = "github-pool"
+  provider_id = "github-gh-provider"
+  sa_mapping = {
+    (google_service_account.sa.account_id) = {
+      sa_name   = google_service_account.sa.name
+      attribute = "attribute.repository/mortezaalizadeh/prototype"
+    }
+  }
+}
---
+++ google/workspaces/modules/workflow_identity_federation/variables.tf
@@ -0,0 +1,4 @@
+variable "project_id" {
+  type        = string
+  description = "The project id to create Workflow Identity Federation pool and Github Service Account"
+}
